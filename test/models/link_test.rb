a675712ff0dca55e6ab0e9971151b5023afe9a47
Alexey Maryshenkov <alekseycrosso@gmail.com>
2025-06-21 01:01:28 +0300

    Update test for Link

--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -6,23 +6,40 @@ describe Link do
   let(:invalid_url) { 'not a url' }
   let(:blank_url)   { '' }
 
-  describe '#valid?' do
-    it 'is valid with a well-formed URL' do
-      link = Link.new(original_url: valid_url)
-      _(link.valid?).must_equal true
-      _(link.errors[:original_url]).must_be_nil
+  describe 'validation' do
+    describe 'URL format validation' do
+      it 'accepts valid URL' do
+        link = Link.new(original_url: valid_url)
+        _(link.valid?).must_equal true
+        _(link.errors[:original_url]).must_be_nil
+      end
+
+      it 'rejects invalid URL format' do
+        link = Link.new(original_url: invalid_url)
+        _(link.valid?).must_equal false
+        _(link.errors[:original_url]).must_include 'Invalid url format'
+      end
+
+      it 'requires URL to be present' do
+        link = Link.new(original_url: blank_url)
+        _(link.valid?).must_equal false
+        _(link.errors[:original_url]).must_include 'is not present'
+      end
     end
 
-    it 'is invalid with a malformed URL' do
-      link = Link.new(original_url: invalid_url)
-      _(link.valid?).must_equal false
-      _(link.errors[:original_url]).must_include 'Invalid url format'
-    end
+    describe 'uniqueness validation' do
+      it 'cannot create two identical original urls' do
+        link1 = Link.new(original_url: valid_url)
+        link1.short_url = 'abcdefg'
+        saved = link1.save
 
-    it 'is invalid with a blank URL' do
-      link = Link.new(original_url: blank_url)
-      _(link.valid?).must_equal false
-      _(link.errors[:original_url]).must_include 'is not present'
+        _(saved).must_be_instance_of Link
+        _(saved.id).wont_be_nil
+
+        link2 = Link.new(original_url: valid_url)
+        link2.short_url = 'abcdefg'
+        _(link2.valid?).must_equal false
+      end
     end
   end
 end
