2c6e7c75ef0e7c259055c74a7eba112b173c9fa1
Alexey Maryshenkov <alekseycrosso@gmail.com>
2025-06-20 21:49:30 +0300

    Initial commit

---
+++ main.rb
@@ -0,0 +1,20 @@
+require 'telegram/bot'
+require 'sequel'
+require 'dotenv'
+Dotenv.load
+
+require_relative 'shortener'
+
+TOKEN = ENV['TOKEN']
+
+Telegram::Bot::Client.run(TOKEN) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start', '/start start'
+      bot.api.send_message(
+        chat_id: message.chat.id,
+        text: "Hello, #{message.from.first_name}! With this bot, you can quickly and easily shorten your long URLs."
+      )
+    end
+  end
+end
