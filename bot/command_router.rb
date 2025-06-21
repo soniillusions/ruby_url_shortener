23d8beac65d61097f22e339f1bcf3a1efe932109
Alexey Maryshenkov <alekseycrosso@gmail.com>
2025-06-21 21:11:32 +0300

    Update Command router

--- bot/command_router.rb
+++ bot/command_router.rb
@@ -5,10 +5,15 @@ module Bot
     end
 
     def route
-      if message.is_a?(Telegram::Bot::Types::CallbackQuery)
-        route_callback_query
-      else
+      case @message
+      when Telegram::Bot::Types::Message
         route_message
+      when Telegram::Bot::Types::CallbackQuery
+        route_callback_query
+      when Telegram::Bot::Types::ChatMemberUpdated
+        handle_chat_member_update
+      else
+        puts "Unknown message type: #{@message.class}"
       end
     end
 
@@ -17,6 +22,8 @@ module Bot
     attr_reader :bot, :message
 
     def route_message
+      return unless message.text
+
       case message.text
       when %r{^/start}        then Commands::StartCommand.new(bot, message)
       when 'Сократить ссылку' then Commands::ShortenRequestCommand.new(bot, message)
@@ -36,6 +43,14 @@ module Bot
         Commands::MainMenuCommand.new(bot, message).call
       else
         bot.api.answer_callback_query(callback_query_id: message.id, text: "Неизвестное действие")
+      end
+    end
+
+    def handle_chat_member_update
+      if message.new_chat_member.status == 'kicked'
+        puts "Bot was blocked by user #{message.from.id}"
+      elsif message.new_chat_member.status == 'member'
+        puts "Bot was added to chat by user #{message.from.id}"
       end
     end
   end
