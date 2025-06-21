00909cb09001e2d8ead5966d5b7789414aa679eb
Alexey Maryshenkov <alekseycrosso@gmail.com>
2025-06-21 21:37:58 +0300

    Fix MainMenu

--- bot/commands/main_menu_command.rb
+++ bot/commands/main_menu_command.rb
@@ -1,10 +1,11 @@
 module Bot::Commands
   class MainMenuCommand < BaseCommand
-    bot.api.answer_callback_query(callback_query_id: message.id)
-    bot.api.send_message(
-      chat_id: message.message.chat.id,
-      text: 'Главное меню:',
-      reply_markup: keyboard
-    )
+    def call
+      bot.api.send_message(
+        chat_id: message.chat.id,
+        text: 'Главное меню:',
+        reply_markup: keyboard
+      )
+    end
   end
 end
