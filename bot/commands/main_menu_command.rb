bf2558e3aef6e862339d86526ad09487589d767a
Alexey Maryshenkov <alekseycrosso@gmail.com>
2025-06-21 20:48:14 +0300

    Update MainMenu commander

--- bot/commands/main_menu_command.rb
+++ bot/commands/main_menu_command.rb
@@ -3,13 +3,21 @@ module Bot::Commands
     def call
       if message.is_a?(Telegram::Bot::Types::CallbackQuery)
         bot.api.answer_callback_query(callback_query_id: message.id)
+        
+        chat_id = message.message.chat.id
 
-        bot.api.delete_message(
-          chat_id: message.from.id,
-          message_id: message.message.message_id
-        ) rescue nil
+        begin
+          bot.api.delete_message(
+            chat_id: chat_id,
+            message_id: message.message.message_id
+          )
+        rescue Telegram::Bot::Exceptions::ResponseError => e
+          puts "Не удалось удалить сообщение: #{e.message}"
+        end
+      else
+        chat_id = message.chat.id
       end
-
+      
       bot.api.send_message(
         chat_id: chat_id,
         text: 'Главное меню:',
