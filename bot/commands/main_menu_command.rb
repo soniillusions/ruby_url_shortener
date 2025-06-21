44e35031da8d4f7391bdae6b1d292f955d6b0d27
Alexey Maryshenkov <alekseycrosso@gmail.com>
2025-06-21 21:34:32 +0300

    Small update for MainMenu command

--- bot/commands/main_menu_command.rb
+++ bot/commands/main_menu_command.rb
@@ -1,11 +1,10 @@
 module Bot::Commands
   class MainMenuCommand < BaseCommand
-    def call
-      bot.api.send_message(
-        chat_id: chat_id,
-        text: 'Главное меню:',
-        reply_markup: keyboard
-      )
-    end
+    bot.api.answer_callback_query(callback_query_id: message.id)
+    bot.api.send_message(
+      chat_id: message.message.chat.id,
+      text: 'Главное меню:',
+      reply_markup: keyboard
+    )
   end
 end
